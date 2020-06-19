c81d7c53f037dbd4b4d592e38461f69ceca76567
Christopher <reinkendigital@gmail.com>
2020-06-19 12:51:50 -0500

    can login with github

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations',
-  omniauth_callbacks: 'users/omniauth_callbacks' }
+  omniauth_callbacks: 'callbacks' }
+        devise_scope :user do
+            get 'login', to: 'devise/sessions#new'
+        end
+
+        devise_scope :user do
+            get 'signup', to: 'devise/registrations#new'
+        end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_153832) do
+ActiveRecord::Schema.define(version: 2020_06_19_172503) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_06_18_153832) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
