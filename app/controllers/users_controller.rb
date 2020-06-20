eff7d24607f60aa8d990496466e83e7d1903ecd8
Christopher <reinkendigital@gmail.com>
2020-06-20 14:11:12 -0500

    fixed some route issues with users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+    before_action :get_user, only: :show
+
+    def index
+        @users = User.all
+    end
+
+    def show
+    end
+
+    private
+
+    def get_user
+        @user = User.find_by(id: params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
         devise_scope :user do
             get 'signup', to: 'devise/registrations#new'
         end
+    resources :users, only: [:index, :show]
+    root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
