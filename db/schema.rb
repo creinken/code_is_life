e0374aaf1f2b2aa05c4a359ca35c908ec77659b2
Christopher <reinkendigital@gmail.com>
2020-06-20 19:44:53 -0500

    added projects index to user's show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_19_172503) do
+ActiveRecord::Schema.define(version: 2020_06_21_002349) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.string "description"
+    t.integer "user_id"
+    t.string "language"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
