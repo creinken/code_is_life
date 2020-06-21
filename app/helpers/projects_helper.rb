19e7727a541af97304f84bc1d645edaf19187d1b
Christopher <reinkendigital@gmail.com>
2020-06-21 13:05:08 -0500

    view project link no matter what but edit link only if owned

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,9 +1,7 @@
 module ProjectsHelper
-    def edit_or_show_project(proj)
+    def edit_project(proj)
         if proj.user_id == current_user.id
             link_to('Edit', edit_project_path(proj))
-        else
-            link_to('View', project_path(proj))
         end
     end
 end
