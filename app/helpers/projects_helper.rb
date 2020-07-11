2d61258194f3540f473691818248570c7c0ac9d6
Christopher <reinkendigital@gmail.com>
2020-07-11 15:43:26 -0500

    fixed routing on projects partial and projects helpers

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,7 +1,9 @@
 module ProjectsHelper
     def edit_project(proj)
-        if proj.user_id == current_user.id
-            link_to('Edit', edit_user_project_path(proj.user_id,proj))
+        if user_signed_in?
+            if proj.user_id == current_user.id
+                link_to('Edit', edit_project_path(proj))
+            end
         end
     end
 end
