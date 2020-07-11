7418963b6f324bb407dba418a60ff54d399e46b0
Christopher <reinkendigital@gmail.com>
2020-07-11 17:54:13 -0500

    nested comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,29 @@
 class CommentsController < ApplicationController
 
+    def index
+        if params[:project_id]
+            @project = Project.find_by(id: params[:project_id])
+            @comments = @project.comments
+            # redirect_to projects_path, if @project.nil?
+        else
+            @comments = Comment.all
+        end
+    end
+
+    def new
+        if params[:project_id]
+            @project = Project.find_by(id: params[:project_id])
+            @comment = @project.comments.new
+        end
+    end
+
     def create
         @commenter = Commenter.find_by(id: params[:comment][:commenter_id])
         @project = Project.find_by(id: params[:comment][:project_id])
         if !params[:comment][:content].empty?
             @commenter.comments.create(comment_params)
         end
-        redirect_to user_project_path(current_user,@project)
+        redirect_to project_path(@project)
     end
 
     def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
         omniauth_callbacks: 'callbacks' }
     resources :users, only: [:index, :show]
     resources :projects do
-        resources :comments, only: [:create, :destroy]
+        resources :comments, only: [:index, :new, :create, :destroy]
     end
+    resources :comments, only: [:index]
     get '/projects/ruby', to: 'projects#show_ruby'
     root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
